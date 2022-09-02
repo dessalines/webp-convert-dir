e29fc29de6048bf54db3ed4bd278c23c3d084af7
Dessalines <tyhou13@gmx.com>
2022-09-02 19:45:29 -0400

    Adding soft error skip

--- webp_convert_dir.sh
+++ webp_convert_dir.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-set -e
-
 find $1 -type f -and \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -print0 | while IFS= read -r -d '' f; do
   FPATH=${f%%.*}
   FEXT=${f##*.}
   WEBPFILE="$FPATH".webp
-  cwebp "$f" -mt -metadata all -q 90 -quiet -o "$WEBPFILE"
+  cwebp "$f" -mt -metadata all -q 90 -quiet -o "$WEBPFILE" || continue
   rm "$f"
   echo "Done converting $WEBPFILE"
 done
